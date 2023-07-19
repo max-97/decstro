c555c3d1d6ff54bac46b0ac3552b0668f0c8792a
Max Braun <maxbraun97@web.de>
2023-07-19 11:07:54 +0200

    Refactor element

--- decstro/__init__.py
+++ decstro/__init__.py
@@ -1,2 +1,3 @@
 from model import Model
+from xpath import XPath
 from element import Element
--- decstro/element.py
+++ decstro/element.py
@@ -1,43 +1,8 @@
-from typing import Union
+from decstro import XPath
 
 
-class Element:
+class Element(XPath):
+
     def __init__(self, expression, /, default=None):
         self.expression = f"./{expression}"
         self.default = default
-
-    def __set_name__(self, owner, name):
-        self.attribute_name = name
-        self.annotation = owner.__annotations__.get(name)
-
-    def __get__(self, instance, owner):
-        value = self.extract(instance.xml)
-        instance.__dict__[self.attribute_name] = value
-        return value
-
-    def extract(self, xml):
-        elements = xml.xpath(self.expression)
-        if elements:
-            if self._is_list_type(self.annotation):
-                return [self.annotation.__args__[0](x) for x in elements]
-            else:
-                if len(elements) > 1:
-                    raise ValueError(f"Multiple elements found for {self.expression} but only one expected.")
-                if self._is_optional_type(self.annotation):
-                    return self.annotation.__args__[0](elements[0])
-                return self.annotation(elements[0])
-        else:
-            if not self._is_optional_type(self.annotation) and self.default is None:
-                raise ValueError(f"""Element {self.expression} not found and no non-None default provided. Check the 
-                XML definition or make the element optional.""")
-
-            return self.default
-
-    @staticmethod
-    def _is_list_type(annotation):
-        return hasattr(annotation, '__origin__') and annotation.__origin__ is list
-
-    @staticmethod
-    def _is_optional_type(annotation):
-        return hasattr(annotation, '__origin__') and annotation.__origin__ is Union and \
-            type(None) in annotation.__args__ and len(annotation.__args__) == 2
