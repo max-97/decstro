cc99ca908478baa6cd2413d40879e52944b6a0a8
Max Braun <maxbraun97@web.de>
2023-08-05 12:40:22 +0200

    Add optional list value

--- decstro/list_element.py
+++ decstro/list_element.py
@@ -1,5 +1,5 @@
 from decstro import XPath
-from decstro.xpath import _is_list_type
+from decstro.xpath import _is_list_type, _is_optional_type
 
 
 class ListElement(XPath):
@@ -11,12 +11,21 @@ class ListElement(XPath):
     def __set_name__(self, owner, name):
         self.attribute_name = name
         self.annotation = owner.__annotations__.get(name)
-        if not _is_list_type(self.annotation):
+
+        if _is_optional_type(self.annotation):
+            if not _is_list_type(self.annotation.__args__[0]):
+                raise AttributeError("Attribute must be a list or optional list.")
+        elif not _is_list_type(self.annotation):
             raise AttributeError("Attribute must be a list.")
 
     def extract(self, xml):
         elements = xml.xpath(self.expression)
         if elements:
+            if _is_optional_type(self.annotation):
+                return [self.annotation.__args__[0].__args__[0](x) for x in elements[0].text.split(self.delimiter)]
             return [self.annotation.__args__[0](x) for x in elements[0].text.split(self.delimiter)]
         else:
+            if not _is_optional_type(self.annotation) and self.default is None:
+                raise ValueError(f"""Element {self.expression} not found and no non-None default provided. Check the 
+                XML definition or make the element optional.""")
             return self.default
--- test/test_list_element.py
+++ test/test_list_element.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import pytest
 
 from decstro import Model, ListElement
@@ -10,4 +12,46 @@ class TestListElement(Model):
 def test_list_element_int():
     xml = "<root><someList>1,2,3,4</someList></root>"
     model = TestListElement(xml)
+    assert model.list_valued_element == [1, 2, 3, 4]
+
+
+def test_empty_list_element_without_default_errors():
+    xml = "<root></root>"
+    with pytest.raises(ValueError):
+        TestListElement(xml)
+
+
+class ListElementString(Model):
+    list_valued_element: list[str] = ListElement("someList", delimiter=",")
+
+
+def test_list_element_string():
+    xml = "<root><someList>1,2,3,4</someList></root>"
+    model = ListElementString(xml)
+    assert model.list_valued_element == ["1", "2", "3", "4"]
+
+
+class ListElementWithDefault(Model):
+    list_valued_element: list[int] = ListElement("someList", delimiter=",", default=[])
+
+
+def test_optional_list_element():
+    xml = "<root></root>"
+    model = ListElementWithDefault(xml)
+    assert model.list_valued_element == []
+
+
+class OptionalListElementWithoutDefault(Model):
+    list_valued_element: Optional[list[int]] = ListElement("someList", delimiter=",")
+
+
+def test_optional_list_element_empty_xml():
+    xml = "<root></root>"
+    model = OptionalListElementWithoutDefault(xml)
+    assert model.list_valued_element is None
+
+
+def test_optional_list_element_with_value():
+    xml = "<root><someList>1,2,3,4</someList></root>"
+    model = OptionalListElementWithoutDefault(xml)
     assert model.list_valued_element == [1, 2, 3, 4]
